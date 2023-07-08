4d824e4363be2e3d7a8013b1be50939b06fa1c55
ryan-tabar <ryan.tabar321@gmail.com>
2023-07-08 16:07:51 +0100

    sorted new lines and displayed results of search employee

--- main.c
+++ main.c
@@ -4,14 +4,15 @@
 
 // Callback function to handle each row of the result set
 int callback(void* data, int argc, char** argv, char** azColName) {
-    for (int i = 0; i < argc; i++) {
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }
+    (void)data; // Cast the unused parameter to void to avoid the warning
     printf("\n");
+    for (int i = 0; i < argc; i++) {
+        printf("%s: %s, ", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
     return 0;
 }
 
-bool executeSQL_query(sqlite3* db, const char* sql) {
+bool executeSQL_query(sqlite3* db, int (*callback)(void *, int, char **, char **), const char* sql) {
     char* err_msg = 0;
     int rc = sqlite3_exec(db, sql, callback, 0, &err_msg);
     
@@ -32,7 +33,7 @@ bool createEmployeeSchema(sqlite3* db) {
         "Position TEXT,"
         "Salary REAL"
         ");";
-    return executeSQL_query(db, sql);
+    return executeSQL_query(db, 0, sql);
 }
 
 bool insertDefaultData(sqlite3* db) {
@@ -43,14 +44,14 @@ bool insertDefaultData(sqlite3* db) {
         "('Ryan Tabar', 'Customer Service', 'Manager', 35000.00),"
         "('Ryan Drown', 'IT', 'Jr Developer', 30000.00),"
         "('Edward Bell', 'Buisness Process', 'Business Analyst', 30000.00);";
-    return executeSQL_query(db, sql);
+    return executeSQL_query(db, 0, sql);
 }
 
 bool searchEmployee(sqlite3* db, const char* name) {
     char sql[100];
-    sprintf(sql, "SELECT Name FROM Employees "
+    sprintf(sql, "SELECT EmployeeID, Name FROM Employees "
                  "WHERE UPPER(Name) LIKE UPPER('%%%s%%');", name);
-    return executeSQL_query(db, sql);
+    return executeSQL_query(db, callback, sql);
 }
 
 enum MenuState {
@@ -62,40 +63,40 @@ enum MenuState {
 };
 
 void handleMenuState(enum MenuState* menuState) {
-    printf("\n----------Main Menu-----------\n");
-    printf("1. Search Employee.\n");
-    printf("2. Add Employee.\n");
-    printf("\nEnter your choice: ");
+    printf("\n\n----------Main Menu-----------");
+    printf("\n\n1. Search Employee.");
+    printf("\n2. Add Employee.");
+    printf("\n\nEnter your choice: ");
     
     int rc = scanf("%d", (int*)menuState);
     if (rc != 1 || *menuState < 0 || *menuState > 5) {
-        printf("Invalid input!\n");
+        printf("Invalid input!");
         *menuState = Main;
     }
 }
 
 void handleSearchEmployeeState(sqlite3* db, enum MenuState* menuState) {
-    printf("\n-------Search Employee--------\n");
+    printf("\n\n-------Search Employee--------");
     // This will print a list of peoples names that match the name inputed to the console
     char name[40];
-    printf("\nType a name: ");
+    printf("\n\nType a name: ");
 
     // Clear the input buffer
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 
     if (scanf("%39[^\n]", name) != 1) {
-        printf("Invalid input!\n");
+        printf("Invalid input!");
         *menuState = SearchEmployee;
     }
-    printf("Searching for %s...\n", name);
+    printf("\nSearching for %s...", name);
     searchEmployee(db, name);
 }
 
 int main() {
     sqlite3* db;
     
-    int rc = sqlite3_open("database.db", &db);
+    int rc = sqlite3_open("employees.db", &db);
     
     if (rc != SQLITE_OK) {
         fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
@@ -113,7 +114,7 @@ int main() {
         return 1;
     }
 
-    printf("\nWelcome to the Employee Management System!\n");
+    printf("\n\nWelcome to the Employee Management System!");
     enum MenuState menuState = Main;
     while (menuState != Exit) {
         switch (menuState) {
